f80c7efeb9dd325ecbb124c143c016d72df2bcf3
irisfaraway <iris.faraway@gmail.com>
2017-11-16 10:51:06 +0000

    Users can log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User
   include Mongoid::Document
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # Don't use :registerable as that's done through the registration process
+  # Use the User database
+  store_in client: "users"
+
   devise :database_authenticatable,
+         :lockable,
          :recoverable,
-         :rememberable,
          :trackable,
          :validatable
 
@@ -18,9 +18,6 @@ class User
   field :reset_password_token,   type: String
   field :reset_password_sent_at, type: Time
 
-  ## Rememberable
-  field :remember_created_at, type: Time
-
   ## Trackable
   field :sign_in_count,      type: Integer, default: 0
   field :current_sign_in_at, type: Time
@@ -28,14 +25,8 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
+  # Lockable
+  field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
+  field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+  field :locked_at,       type: Time
 end
