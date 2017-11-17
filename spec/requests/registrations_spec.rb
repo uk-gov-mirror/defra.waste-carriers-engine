a37ced5c4d415a589810df01df3c02c606968a38
irisfaraway <iris.faraway@gmail.com>
2017-11-17 09:27:24 +0000

    Users must log in to access registrations

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,17 +1,55 @@
 require "rails_helper"
 
 RSpec.describe "Registrations", type: :request do
-  describe "GET /registrations" do
-    it "returns a success response" do
-      get registrations_path
-      expect(response).to have_http_status(200)
+  context "when a user is signed in" do
+    before(:each) do
+      user = create(:user)
+      sign_in(user)
+    end
+
+    describe "GET /registrations" do
+      it "returns a success response" do
+        get registrations_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /registrations/new" do
+      it "returns a success response" do
+        get new_registration_path
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
-  describe "GET /registrations/new" do
-    it "returns a success response" do
-      get new_registration_path
-      expect(response).to have_http_status(200)
+  context "when a user is not signed in" do
+    before(:each) do
+      user = create(:user)
+      sign_out(user)
+    end
+
+    describe "GET /registrations" do
+      it "returns a 302 response" do
+        get registrations_path
+        expect(response).to have_http_status(302)
+      end
+
+      it "redirects to the sign in page" do
+        get registrations_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe "GET /registrations/new" do
+      it "returns a 302 response" do
+        get new_registration_path
+        expect(response).to have_http_status(302)
+      end
+
+      it "redirects to the sign in page" do
+        get registrations_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
