1d1bab4487d73c6a33db37336c8f5a15f88e1fe0
Iris Faraway <iris.faraway@gmail.com>
2018-01-16 10:52:26 +0000

    Improve database seeding (#37)
    https://eaflood.atlassian.net/browse/WC-167
    
    We need some seed data for the database for the dev environment and for running acceptance tests against. This change switches from loading all seeds in a single script to storing seeds as multiple JSON files and then loading them. This approach should make it easier to add and modify seeds.
    
    Seeds with regIdentifiers are loaded first. Then ones without regIdentifiers are loaded and assigned new regIdentifiers to 'fill in the gaps'.
    
    This change also includes a refactor of the CanGenerateRegIdentifier concern to continue incrementing the counter if it would clash with a regIdentifier which was added out of sequence.

--- app/models/concerns/can_generate_reg_identifier.rb
+++ app/models/concerns/can_generate_reg_identifier.rb
@@ -5,17 +5,24 @@ module CanGenerateRegIdentifier
     # Use the existing reg_identifier if one is already set, eg. through seeding
     return if reg_identifier
 
-    # Get the counter for reg_identifiers, or create it if it doesn't exist
-    counter = Counter.where(_id: "regid").first || Counter.create(_id: "regid", seq: 0)
-
-    # Increment the counter and get the updated value
-    counter.increment
-    number = counter.seq
-
     self.reg_identifier = if tier == "UPPER"
-                            "CBDU#{number}"
+                            "CBDU#{latest_counter}"
                           elsif tier == "LOWER"
-                            "CBDL#{number}"
+                            "CBDL#{latest_counter}"
                           end
+  end
+
+  private
+
+  def latest_counter
+    # Get the counter for reg_identifiers, or create it if it doesn't exist
+    counter = Counter.where(_id: "regid").first || Counter.create(_id: "regid", seq: 1)
+
+    # Increment the counter until no reg_identifier is using it
+    while Registration.where(reg_identifier: /CBD[U|L]#{counter.seq}/).exists?
+      counter.increment
+    end
+
+    counter.seq
   end
 end
