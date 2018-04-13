68ee35242c90f6b0a3be1f55b19b2ba0a2be3f74
Iris Faraway <iris.faraway@gmail.com>
2018-04-13 17:23:17 +0100

    Ensure each renewals service page has a unique title (#126)
    https://eaflood.atlassian.net/browse/WC-194
    
    We need to go through each page in the renewals service and set a title for each page that is descriptive and unique. Otherwise all forms have the same title, which is confusing.
    
    Specific titles (like "Company name") can be set in the view or in the language file. All titles are appended with the service name and GOV.UK. If no unique title is specified, we fall back to a default for the service.
    
    The titles themselves may need a review from a content person.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,24 @@
 module ApplicationHelper
-
   def self.feedback_survey_url(current_title)
     survey_url = "https://www.smartsurvey.co.uk/s/floodexemptions/?"
     survey_params = { referringpage: current_title }
     survey_url + survey_params.to_query
   end
 
+  def title
+    title_elements = [title_text, "Register as a waste carrier", "GOV.UK"]
+    # Remove empty elements, for example if no specific title is set
+    title_elements.delete_if(&:empty?)
+    title_elements.join(" - ")
+  end
+
+  private
+
+  def title_text
+    # Check if the title is set in the view (we do this for High Voltage pages)
+    return content_for :title if content_for?(:title)
+    # Otherwise, look up translation key based on controller path, action name and .title
+    # Solution from https://coderwall.com/p/a1pj7w/rails-page-titles-with-the-right-amount-of-magic
+    t("#{controller_path.tr('/', '.')}.#{action_name}.title", default: "")
+  end
 end
