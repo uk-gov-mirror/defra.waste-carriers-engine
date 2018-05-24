1a6d2c227b7a80a0c02722ce7c2b22da76f6b1e7
Iris Faraway <iris.faraway@gmail.com>
2018-05-24 10:44:18 +0100

    Fix missing error messages for postcode validation (#169)
    There was a missing 's'.

--- app/validators/postcode_validator.rb
+++ app/validators/postcode_validator.rb
@@ -37,6 +37,6 @@ class PostcodeValidator < ActiveModel::EachValidator
 
   def error_message(record, attribute, error)
     class_name = record.class.to_s.underscore
-    I18n.t("activemodel.errors.models.#{class_name}.attribute.#{attribute}.#{error}")
+    I18n.t("activemodel.errors.models.#{class_name}.attributes.#{attribute}.#{error}")
   end
 end
