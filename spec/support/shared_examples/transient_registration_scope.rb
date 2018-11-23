9de9bd7703d79746133c59da718e86aa82074a7d
Iris Faraway <iris.faraway@gmail.com>
2018-11-23 12:59:46 +0000

    Refactor scope tests for consistency (#305)
    * Refactor scope tests for consistency
    
    Update our specs for transient_registration scopes so we are testing them in a consistent way.
    
    * Change 'context' to 'describe'
    
    As these blocks describe the method, rather than the context in which it is being called.
    
    * Use let to reduce duplication in test objects
    
    Several scope specs were creating near-identical objects in the individual specs. We can reduce duplication by specifying these using `let`.
    
    * Reuse existing test object for reg_identifier search spec
    
    We can use any transient_registration with a unique reg_identifier to test this, so `in_progress_renewal` fits the bill.
    
    * Move example objects for name search test into let blocks
    
    This is a precursor to further refactoring, so we can test the expectations individually.
    
    Also add some extra expectations that the results include the test objects, rather than just checking the length.
    
    * Move postcode test objects into let block
    
    Then test that the results include the matching object and do not include the non-matching one.
    
    Also a precursor to testing expectations separately.
    
    * Define scope for filter specs in describe block
    
    Use `let` to define the scope we are testing once, just inside the `describe` block. This means the individual specs have less duplication and are easier to read.
    
    * Define search_term scope once and change term in context
    
    As we did with the filters, define the scope for `search_term` using a `let` block right inside the `define` block. We provide a default for term, but can use additional `let` blocks to modify this depending on the context.
    
    * Split expectations into separate blocks
    
    This means we can run tests at a more granular level, and means the format is more consistent with our other scope tests.
    
    * Always define term just inside context block
    
    A minor reordering, but makes the tests a bit easier to read.

--- spec/support/shared_examples/transient_registration_scope.rb
+++ spec/support/shared_examples/transient_registration_scope.rb
@@ -1,108 +1,137 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples "TransientRegistration named scopes" do
-  context "#search_term" do
+  let(:in_progress_renewal) do
+    create(:transient_registration, :has_required_data)
+  end
+
+  let(:submitted_renewal) do
+    create(:transient_registration,
+           :has_required_data,
+           workflow_state: :renewal_received_form)
+  end
+
+  let(:pending_payment_renewal) do
+    create(:transient_registration,
+           :has_required_data,
+           :has_unpaid_balance,
+           workflow_state: :renewal_received_form)
+  end
+
+  let(:pending_approval_renewal) do
+    create(:transient_registration,
+           :has_required_data,
+           :requires_conviction_check,
+           workflow_state: :renewal_received_form)
+  end
+
+  describe "#search_term" do
+    let(:term) { nil }
+    let(:scope) { WasteCarriersEngine::TransientRegistration.search_term(term) }
+
     it "returns everything when no search term is given" do
-      expect(WasteCarriersEngine::TransientRegistration.search_term(nil).length).to eq(WasteCarriersEngine::TransientRegistration.all.length)
+      expect(scope.length).to eq(WasteCarriersEngine::TransientRegistration.all.length)
     end
 
-    it "returns only matching renewal when a reg. identifier is given" do
-      matching_renewal = create(:transient_registration, :has_required_data)
-      create(:transient_registration, :has_required_data)
-      results = WasteCarriersEngine::TransientRegistration.search_term(
-        matching_renewal.reg_identifier
-      )
+    context "when the search term is a reg_identifier" do
+      let(:term) { in_progress_renewal.reg_identifier }
 
-      expect(results).to include(matching_renewal)
-      expect(results.length).to eq(1)
+      it "returns renewals with a matching reg_identifier" do
+        expect(scope).to include(in_progress_renewal)
+      end
+
+      it "does not return others" do
+        expect(scope).not_to include(submitted_renewal)
+      end
     end
 
-    it "returns all matching renewals when a general name is given" do
-      create(:transient_registration, :has_required_data, company_name: "Stan Lee Waste Company")
-      create(:transient_registration, :has_required_data, last_name: "Lee")
-      non_matching_renewal = create(:transient_registration, :has_required_data, company_name: "Excelsior Waste")
-      results = WasteCarriersEngine::TransientRegistration.search_term("lee")
+    context "when the search term is a name" do
+      let(:term) { "Lee" }
 
-      expect(results).not_to include(non_matching_renewal)
-      expect(results.length).to eq(2)
+      let(:matching_company_name_renewal) do
+        create(:transient_registration, :has_required_data, company_name: "Stan Lee Waste Company")
+      end
+
+      let(:matching_person_name_renewal) do
+        create(:transient_registration, :has_required_data, last_name: "Lee")
+      end
+
+      it "returns renewals with a matching company_name" do
+        expect(scope).to include(matching_company_name_renewal)
+      end
+
+      it "returns renewals with a matching last_name" do
+        expect(scope).to include(matching_person_name_renewal)
+      end
+
+      it "does not return others" do
+        expect(scope).not_to include(in_progress_renewal)
+      end
     end
 
-    it "returns all matching renewals when a postcode is given" do
-      ["SW1A 2AA", "BS1 5AH"].each do |postcode|
-        address = build(:address, postcode: postcode)
+    context "when the search term is a postcode" do
+      let(:term) { "SW1A 2AA" }
+
+      let(:matching_postcode_renewal) do
+        address = build(:address, postcode: term)
         create(:transient_registration, :has_required_data, addresses: [address])
       end
 
-      results = WasteCarriersEngine::TransientRegistration.search_term("SW1A 2AA")
+      it "returns renewals with a matching postcode" do
+        expect(scope).to include(matching_postcode_renewal)
+      end
 
-      expect(results.first.addresses[0].postcode).to eq("SW1A 2AA")
-      expect(results.length).to eq(1)
+      it "does not return others" do
+        expect(scope).not_to include(in_progress_renewal)
+      end
     end
   end
 
-  context "#in_progress" do
+  describe "#in_progress" do
+    let(:scope) { WasteCarriersEngine::TransientRegistration.in_progress }
+
     it "returns in progress renewals when they exist" do
-      in_progress_renewal = create(:transient_registration, :has_required_data)
-      expect(WasteCarriersEngine::TransientRegistration.in_progress).to include(in_progress_renewal)
+      expect(scope).to include(in_progress_renewal)
     end
 
     it "does not return submitted renewals" do
-      submitted_renewal = create(
-        :transient_registration,
-        :has_required_data,
-        workflow_state: :renewal_complete_form
-      )
-      expect(WasteCarriersEngine::TransientRegistration.in_progress).not_to include(submitted_renewal)
+      expect(scope).not_to include(submitted_renewal)
     end
   end
 
-  context "#submitted" do
+  describe "#submitted" do
+    let(:scope) { WasteCarriersEngine::TransientRegistration.submitted }
+
     it "returns submitted renewals" do
-      submitted_renewal = create(
-        :transient_registration,
-        :has_required_data,
-        workflow_state: :renewal_complete_form
-      )
-      expect(WasteCarriersEngine::TransientRegistration.submitted).to include(submitted_renewal)
+      expect(scope).to include(submitted_renewal)
     end
 
     it "does not return in progress renewals" do
-      in_progress_renewal = create(:transient_registration, :has_required_data)
-      expect(WasteCarriersEngine::TransientRegistration.submitted).not_to include(in_progress_renewal)
+      expect(scope).not_to include(in_progress_renewal)
     end
   end
 
-  context "#pending_payment" do
+  describe "#pending_payment" do
+    let(:scope) { WasteCarriersEngine::TransientRegistration.pending_payment }
+
     it "returns renewals pending payment" do
-      pending_payment_renewal = create(
-        :transient_registration,
-        :has_required_data,
-        :has_unpaid_balance,
-        workflow_state: :renewal_complete_form
-      )
-      expect(WasteCarriersEngine::TransientRegistration.pending_payment).to include(pending_payment_renewal)
+      expect(scope).to include(pending_payment_renewal)
     end
 
     it "does not return others" do
-      in_progress_renewal = create(:transient_registration, :has_required_data)
-      expect(WasteCarriersEngine::TransientRegistration.pending_payment).not_to include(in_progress_renewal)
+      expect(scope).not_to include(in_progress_renewal)
     end
   end
 
-  context "#pending_approval" do
+  describe "#pending_approval" do
+    let(:scope) { WasteCarriersEngine::TransientRegistration.pending_approval }
+
     it "returns renewals pending conviction approval" do
-      pending_approval_renewal = create(
-        :transient_registration,
-        :has_required_data,
-        :requires_conviction_check,
-        workflow_state: :renewal_complete_form
-      )
-      expect(WasteCarriersEngine::TransientRegistration.pending_approval).to include(pending_approval_renewal)
+      expect(scope).to include(pending_approval_renewal)
     end
 
     it "does not return others" do
-      in_progress_renewal = create(:transient_registration, :has_required_data)
-      expect(WasteCarriersEngine::TransientRegistration.pending_approval).not_to include(in_progress_renewal)
+      expect(scope).not_to include(in_progress_renewal)
     end
   end
 
