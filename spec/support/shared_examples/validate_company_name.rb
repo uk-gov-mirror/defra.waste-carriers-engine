fc1acadc11fae5e890a4ba0eb9610dbc41da055a
cintamani <cintamani.puddu@gmail.com>
2019-10-22 15:21:34 +0100

    Forms refactoring - Remove temporary hack in specs (#531)
    From: https://eaflood.atlassian.net/browse/RUBY-693
    
    Since we now use delegate all over the place, we don't need this hack any more

--- spec/support/shared_examples/validate_company_name.rb
+++ spec/support/shared_examples/validate_company_name.rb
@@ -13,12 +13,7 @@ RSpec.shared_examples "validate company_name" do |form_factory|
 
     context "when a company_name is blank" do
       before do
-        # TODO: Temporary refactoring code
-        if form.respond_to?(:company_name=)
-          form.company_name = ""
-        else
-          form.transient_registration.company_name = ""
-        end
+        form.transient_registration.company_name = ""
       end
 
       it "is not valid" do
@@ -28,12 +23,7 @@ RSpec.shared_examples "validate company_name" do |form_factory|
 
     context "when a company name is too long" do
       before do
-        # TODO: Temporary refactoring code
-        if form.respond_to?(:company_name=)
-          form.company_name = "ak67inm5ijij85w3a7gck67iloe2k98zyk01607xbhfqzznr4kbl5tuypqlbrpdvwqcup8ij9o2b0ryquhdmv5716s9zia3vz184g5vkhnk8869whwulmkqd47tqxveifrsg4wxpi0dbygo42k1ujdj8w4we2uvfvoamovk0u8ru5bk5esrxwxdue8sh7e03e3popgl2yzjvs5vk49xt5qtxaijdafdnlgc468jj4k21g3jumtsxc9nup8bgu83viakj0x6c47r7zfzxrr2nl3rn47v86odk6ra0e0dic7g7"
-        else
-          form.transient_registration.company_name = "ak67inm5ijij85w3a7gck67iloe2k98zyk01607xbhfqzznr4kbl5tuypqlbrpdvwqcup8ij9o2b0ryquhdmv5716s9zia3vz184g5vkhnk8869whwulmkqd47tqxveifrsg4wxpi0dbygo42k1ujdj8w4we2uvfvoamovk0u8ru5bk5esrxwxdue8sh7e03e3popgl2yzjvs5vk49xt5qtxaijdafdnlgc468jj4k21g3jumtsxc9nup8bgu83viakj0x6c47r7zfzxrr2nl3rn47v86odk6ra0e0dic7g7"
-        end
+        form.transient_registration.company_name = "ak67inm5ijij85w3a7gck67iloe2k98zyk01607xbhfqzznr4kbl5tuypqlbrpdvwqcup8ij9o2b0ryquhdmv5716s9zia3vz184g5vkhnk8869whwulmkqd47tqxveifrsg4wxpi0dbygo42k1ujdj8w4we2uvfvoamovk0u8ru5bk5esrxwxdue8sh7e03e3popgl2yzjvs5vk49xt5qtxaijdafdnlgc468jj4k21g3jumtsxc9nup8bgu83viakj0x6c47r7zfzxrr2nl3rn47v86odk6ra0e0dic7g7"
       end
 
       it "is not valid" do
