689a8a277658b7bc7452e5980ebcc59bdfd5acf5
Iris Faraway <iris.faraway@gmail.com>
2020-04-24 09:39:12 +0100

    Refactor cease or revoke workflow specs to use shared examples (#803)
    This PR refactors our specs for the cease or revoke workflow to use the "has next transition" and "has back transition" shared examples.

--- spec/models/waste_carriers_engine/ceased_or_revoked_registration_workflow/cease_or_revoke_form_spec.rb
+++ spec/models/waste_carriers_engine/ceased_or_revoked_registration_workflow/cease_or_revoke_form_spec.rb
@@ -4,18 +4,12 @@ require "rails_helper"
 
 module WasteCarriersEngine
   RSpec.describe CeasedOrRevokedRegistration do
-    subject(:ceased_or_revoked_registration) { build(:ceased_or_revoked_registration) }
+    subject(:ceased_or_revoked_registration) { build(:ceased_or_revoked_registration, workflow_state: "cease_or_revoke_form") }
 
     describe "#workflow_state" do
       context ":cease_or_revoke_form state transitions" do
         context "on next" do
-          it "can transition from a :cease_or_revoke_form state to a :ceased_or_revoked_confirm_form" do
-            ceased_or_revoked_registration.workflow_state = :cease_or_revoke_form
-
-            ceased_or_revoked_registration.next
-
-            expect(ceased_or_revoked_registration.workflow_state).to eq("ceased_or_revoked_confirm_form")
-          end
+          include_examples "has next transition", next_state: "ceased_or_revoked_confirm_form"
         end
       end
     end
--- spec/models/waste_carriers_engine/ceased_or_revoked_registration_workflow/ceased_or_revoked_confirm_form_spec.rb
+++ spec/models/waste_carriers_engine/ceased_or_revoked_registration_workflow/ceased_or_revoked_confirm_form_spec.rb
@@ -4,28 +4,16 @@ require "rails_helper"
 
 module WasteCarriersEngine
   RSpec.describe CeasedOrRevokedRegistration do
-    subject(:ceased_or_revoked_registration) { build(:ceased_or_revoked_registration) }
+    subject(:ceased_or_revoked_registration) { build(:ceased_or_revoked_registration, workflow_state: "ceased_or_revoked_confirm_form") }
 
     describe "#workflow_state" do
       context ":ceased_or_revoked_confirm_form state transitions" do
         context "on next" do
-          it "can transition from a :ceased_or_revoked_confirm_form state to a :ceased_or_revoked_completed_form" do
-            ceased_or_revoked_registration.workflow_state = :ceased_or_revoked_confirm_form
-
-            ceased_or_revoked_registration.next
-
-            expect(ceased_or_revoked_registration.workflow_state).to eq("ceased_or_revoked_completed_form")
-          end
+          include_examples "has next transition", next_state: "ceased_or_revoked_completed_form"
         end
 
         context "on back" do
-          it "can transition from a :ceased_or_revoked_confirm_form state to a :cease_or_revoke_form" do
-            ceased_or_revoked_registration.workflow_state = :ceased_or_revoked_confirm_form
-
-            ceased_or_revoked_registration.back
-
-            expect(ceased_or_revoked_registration.workflow_state).to eq("cease_or_revoke_form")
-          end
+          include_examples "has back transition", previous_state: "cease_or_revoke_form"
         end
       end
     end
