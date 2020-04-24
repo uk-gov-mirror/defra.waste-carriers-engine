6422f2feaf998f48dabfe5fc1137c5f8f0e3f7b6
Iris Faraway <iris.faraway@gmail.com>
2020-04-24 09:25:49 +0100

    Refactor copy cards workflow specs to use shared examples (#804)
    This PR refactors our specs for the copy card order workflow to use the "has next transition" and "has back transition" shared examples.

--- spec/models/waste_carriers_engine/order_copy_cards_registration_workflow_state/copy_cards_bank_transfer_form_spec.rb
+++ spec/models/waste_carriers_engine/order_copy_cards_registration_workflow_state/copy_cards_bank_transfer_form_spec.rb
@@ -4,28 +4,16 @@ require "rails_helper"
 
 module WasteCarriersEngine
   RSpec.describe OrderCopyCardsRegistration do
-    subject(:order_copy_cards_registration) { build(:order_copy_cards_registration) }
+    subject { build(:order_copy_cards_registration, workflow_state: "copy_cards_bank_transfer_form") }
 
     describe "#workflow_state" do
       context ":copy_cards_bank_transfer_form state transitions" do
         context "on next" do
-          it "can transition from a :copy_cards_bank_transfer_form state to a :copy_cards_order_completed_form" do
-            order_copy_cards_registration.workflow_state = :copy_cards_bank_transfer_form
-
-            order_copy_cards_registration.next
-
-            expect(order_copy_cards_registration.workflow_state).to eq("copy_cards_order_completed_form")
-          end
+          include_examples "has next transition", next_state: "copy_cards_order_completed_form"
         end
 
         context "on back" do
-          it "can transition from a :copy_cards_bank_transfer_form state to a :copy_cards_payment_form" do
-            order_copy_cards_registration.workflow_state = :copy_cards_bank_transfer_form
-
-            order_copy_cards_registration.back
-
-            expect(order_copy_cards_registration.workflow_state).to eq("copy_cards_payment_form")
-          end
+          include_examples "has back transition", previous_state: "copy_cards_payment_form"
         end
       end
     end
--- spec/models/waste_carriers_engine/order_copy_cards_registration_workflow_state/copy_cards_payment_form_spec.rb
+++ spec/models/waste_carriers_engine/order_copy_cards_registration_workflow_state/copy_cards_payment_form_spec.rb
@@ -4,42 +4,26 @@ require "rails_helper"
 
 module WasteCarriersEngine
   RSpec.describe OrderCopyCardsRegistration do
-    subject(:order_copy_cards_registration) { build(:order_copy_cards_registration) }
+    subject { build(:order_copy_cards_registration, workflow_state: "copy_cards_payment_form") }
 
     describe "#workflow_state" do
-      context ":copy_cards_payment_form state transitions" do
+      context ":worldpay_form state transitions" do
         context "on next" do
           context "when the method is paying by card" do
-            it "can transition from :copy_cards_payment_form to :worldpay_form" do
-              order_copy_cards_registration.temp_payment_method = "card"
-              order_copy_cards_registration.workflow_state = :copy_cards_payment_form
+            before { subject.temp_payment_method = "card" }
 
-              order_copy_cards_registration.next
-
-              expect(order_copy_cards_registration.workflow_state).to eq("worldpay_form")
-            end
+            include_examples "has next transition", next_state: "worldpay_form"
           end
 
           context "when the method is not paying by card" do
-            it "can transition from :copy_cards_payment_form to :copy_cards_bank_transfer_form" do
-              order_copy_cards_registration.temp_payment_method = "foo"
-              order_copy_cards_registration.workflow_state = :copy_cards_payment_form
+            before { subject.temp_payment_method = "bank_transfer" }
 
-              order_copy_cards_registration.next
-
-              expect(order_copy_cards_registration.workflow_state).to eq("copy_cards_bank_transfer_form")
-            end
+            include_examples "has next transition", next_state: "copy_cards_bank_transfer_form"
           end
         end
 
         context "on back" do
-          it "can transition from a :copy_cards_payment_form state to a :copy_cards_form" do
-            order_copy_cards_registration.workflow_state = :copy_cards_payment_form
-
-            order_copy_cards_registration.back
-
-            expect(order_copy_cards_registration.workflow_state).to eq("copy_cards_form")
-          end
+          include_examples "has back transition", previous_state: "copy_cards_form"
         end
       end
     end
