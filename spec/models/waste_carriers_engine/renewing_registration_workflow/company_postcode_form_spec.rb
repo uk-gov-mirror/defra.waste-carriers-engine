3f855c73d21dd05eecaa80d579d45f2c43e5d542
Iris Faraway <iris.faraway@gmail.com>
2020-04-27 14:40:46 +0100

    Use shared examples for renewal address workflow specs (#813)
    This PR replaces our individual specs for testing the renewal address workflows with the shared examples we use elsewhere.

--- spec/models/waste_carriers_engine/renewing_registration_workflow/company_postcode_form_spec.rb
+++ spec/models/waste_carriers_engine/renewing_registration_workflow/company_postcode_form_spec.rb
@@ -3,27 +3,12 @@
 require "rails_helper"
 
 module WasteCarriersEngine
-  RSpec.describe RenewingRegistration, type: :model do
+  RSpec.describe RenewingRegistration do
     describe "#workflow_state" do
-      context "when a RenewingRegistration's state is :company_postcode_form" do
-        let(:transient_registration) do
-          create(:renewing_registration,
-                 :has_required_data,
-                 workflow_state: "company_postcode_form")
-        end
-
-        it "changes to :company_name_form after the 'back' event" do
-          expect(transient_registration).to transition_from(:company_postcode_form).to(:company_name_form).on_event(:back)
-        end
-
-        it "changes to :company_address_form after the 'next' event" do
-          expect(transient_registration).to transition_from(:company_postcode_form).to(:company_address_form).on_event(:next)
-        end
-
-        it "changes to :company_address_manual_form after the 'skip_to_manual_address' event" do
-          expect(transient_registration).to transition_from(:company_postcode_form).to(:company_address_manual_form).on_event(:skip_to_manual_address)
-        end
-      end
+      it_behaves_like "a postcode transition",
+                      previous_state: :company_name_form,
+                      address_type: "company",
+                      factory: :renewing_registration
     end
   end
 end
--- spec/models/waste_carriers_engine/renewing_registration_workflow/contact_postcode_form_spec.rb
+++ spec/models/waste_carriers_engine/renewing_registration_workflow/contact_postcode_form_spec.rb
@@ -3,27 +3,12 @@
 require "rails_helper"
 
 module WasteCarriersEngine
-  RSpec.describe RenewingRegistration, type: :model do
+  RSpec.describe RenewingRegistration do
     describe "#workflow_state" do
-      context "when a RenewingRegistration's state is :contact_postcode_form" do
-        let(:transient_registration) do
-          create(:renewing_registration,
-                 :has_required_data,
-                 workflow_state: "contact_postcode_form")
-        end
-
-        it "changes to :contact_email_form after the 'back' event" do
-          expect(transient_registration).to transition_from(:contact_postcode_form).to(:contact_email_form).on_event(:back)
-        end
-
-        it "changes to :contact_address_form after the 'next' event" do
-          expect(transient_registration).to transition_from(:contact_postcode_form).to(:contact_address_form).on_event(:next)
-        end
-
-        it "changes to :contact_address_manual_form after the 'skip_to_manual_address' event" do
-          expect(transient_registration).to transition_from(:contact_postcode_form).to(:contact_address_manual_form).on_event(:skip_to_manual_address)
-        end
-      end
+      it_behaves_like "a postcode transition",
+                      previous_state: :contact_email_form,
+                      address_type: "contact",
+                      factory: :renewing_registration
     end
   end
 end
