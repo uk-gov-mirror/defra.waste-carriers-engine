2e42dfb1eb323767bd7f0699b2469b2d49e773f4
cintamani <cintamani.puddu@gmail.com>
2020-07-02 09:20:22 +0100

    Promote little FeatureToggle to be a proper Model (#872)
    https://eaflood.atlassian.net/browse/RUBY-1118
    
    Given that little `FeatureToggle` has behaved very well in the past few months, we decided to upgrade him to be a proper Model 😁 
    
    This model will be used by the new [defra-ruby-features](https://github.com/DEFRA/defra-ruby-features) engine. Toggles will be persisted in the database.
    
    Things to notice are, in order to handle the fact that the model won't have persisted data about existing toggles which were previously persisted in a yml file, the code will handle that case by redirecting the query for a toggle status to the content of the yml file.
    
    After we deploy we'll just need to take our time creating the toggles through the back-office interface. In the meantime, everything will work just fine.
    
    Once deployed and the existing features are migrated from the yml file to the database we can begin to clean up the code. But we still think this is a cleaner solution than a one-time rake task and Jenkins job.

--- app/models/waste_carriers_engine/feature_toggle.rb
+++ app/models/waste_carriers_engine/feature_toggle.rb
@@ -5,11 +5,20 @@ require "erb"
 
 module WasteCarriersEngine
   class FeatureToggle
+    include Mongoid::Document
+
+    # Use the FeatureToggles database
+    store_in collection: "feature_toggles"
+
+    field :key, type: String
+    field :active, type: Boolean, default: false
+
     def self.active?(feature_name)
-      feature_toggles[feature_name] && (
-        feature_toggles[feature_name][:active] == true ||
-        feature_toggles[feature_name][:active] == "true"
-      )
+      from_database = where(key: feature_name).first
+
+      return from_file(feature_name) unless from_database.present?
+
+      from_database.active
     end
 
     class << self
@@ -20,6 +29,13 @@ module WasteCarriersEngine
         @@feature_toggles ||= load_feature_toggles
       end
       # rubocop:enable Style/ClassVars
+
+      def from_file(feature_name)
+        feature_toggles[feature_name] && (
+          feature_toggles[feature_name][:active] == true ||
+          feature_toggles[feature_name][:active] == "true"
+        )
+      end
 
       def load_feature_toggles
         HashWithIndifferentAccess.new(YAML.safe_load(ERB.new(File.read(file_path)).result))
---
+++ spec/factories/feature_toggle.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :feature_toggle, class: WasteCarriersEngine::FeatureToggle do
+    key { "test-feature" }
+
+    active { false }
+  end
+end
