bf2d1eed53339e2e79862537eda78b5d18a9e4f0
Iris Faraway <iris.faraway@gmail.com>
2020-10-05 16:54:57 +0100

    Fix hardcoded links to old frontend (#914)
    https://eaflood.atlassian.net/browse/RUBY-1223
    
    We spotted that some pages (and an email) had hardcoded links to the old frontend. Since the frontend is being removed, we need to update these to go to the front office instead.
    
    One of these instances was no longer being used in the code at all, so I have removed it.

--- app/forms/waste_carriers_engine/renewal_complete_form.rb
+++ app/forms/waste_carriers_engine/renewal_complete_form.rb
@@ -4,7 +4,7 @@ module WasteCarriersEngine
   class RenewalCompleteForm < ::WasteCarriersEngine::BaseForm
     include CannotSubmit
 
-    attr_accessor :certificate_link, :contact_email, :projected_renewal_end_date, :registration_type
+    attr_accessor :contact_email, :projected_renewal_end_date, :registration_type
 
     def self.can_navigate_flexibly?
       false
@@ -13,21 +13,9 @@ module WasteCarriersEngine
     def initialize(transient_registration)
       super
 
-      self.certificate_link = build_certificate_link
       self.contact_email = transient_registration.contact_email
       self.projected_renewal_end_date = transient_registration.projected_renewal_end_date
       self.registration_type = transient_registration.registration_type
-    end
-
-    private
-
-    def build_certificate_link
-      registration = @transient_registration.registration
-      return unless registration.present?
-
-      id = registration.id
-      root = Rails.configuration.wcrs_frontend_url
-      "#{root}/registrations/#{id}/view"
     end
   end
 end
