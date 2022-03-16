64480b4ee089683322e1385fbf65b300f7735c40
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-03-16 16:55:14 +0000

    Make sole trader business name optional
    https://eaflood.atlassian.net/browse/RUBY-1806

--- app/validators/waste_carriers_engine/company_name_validator.rb
+++ app/validators/waste_carriers_engine/company_name_validator.rb
@@ -5,7 +5,11 @@ module WasteCarriersEngine
     include CanAddValidationErrors
 
     def validate_each(record, attribute, value)
-      return false unless value_is_present?(record, attribute, value)
+      if attribute == :company_name
+        return false unless valid_company_name?(record, attribute, value)
+      else
+        return false unless value_is_present?(record, attribute, value)
+      end
 
       value_is_not_too_long?(record, attribute, value)
     end
@@ -14,8 +18,6 @@ module WasteCarriersEngine
 
     def value_is_present?(record, attribute, value)
       return true if value.present?
-
-      return true if attribute == :company_name && record.registered_company_name.present?
 
       add_validation_error(record, attribute, :blank)
       false
@@ -26,6 +28,15 @@ module WasteCarriersEngine
 
       add_validation_error(record, attribute, :too_long)
       false
+    end
+
+    def valid_company_name?(record, attribute, value)
+      if (%w[limitedCompany limitedLiabilityPartnership].include? record.business_type) &&
+         !record.registered_company_name.present?
+        return value_is_present?(record, attribute, value)
+      end
+
+      true
     end
   end
 end
---
+++ spec/validators/waste_carriers_engine/company_name_validator.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Test
+  CompanyNameValidatable = Struct.new(:company_name) do
+    include ActiveModel::Validations
+
+    attr_reader :company_name
+    attr_reader :registered_company_name
+    attr_reader :business_type
+
+    validates_with WasteCarriersEngine::CompanyNameValidator, attributes: [:company_name]
+  end
+end
+
+module WasteCarriersEngine
+  # rubocop:disable Metrics/BlockLength
+  RSpec.describe CompanyNameValidator do
+    subject { Test::CompanyNameValidatable.new }
+
+    RSpec.shared_examples "is valid" do
+      it "passes the validity check" do
+        expect(subject).to be_valid
+      end
+    end
+
+    RSpec.shared_examples "is not valid" do
+      it "does not pass the validity check" do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    RSpec.shared_examples "business name is required" do
+      context "with a business name" do
+        before { allow(subject).to receive(:company_name).and_return(Faker::Company.name) }
+        it_behaves_like "is valid"
+      end
+      context "without a business name" do
+        before { allow(subject).to receive(:company_name).and_return(nil) }
+        it_behaves_like "is not valid"
+      end
+      context "with a blank business name" do
+        before { allow(subject).to receive(:company_name).and_return("") }
+        it_behaves_like "is not valid"
+      end
+    end
+
+    RSpec.shared_examples "business name is optional" do
+      context "with a business name" do
+        before { allow(subject).to receive(:company_name).and_return(Faker::Company.name) }
+        it_behaves_like "is valid"
+      end
+      context "without a business name" do
+        before { allow(subject).to receive(:company_name).and_return(nil) }
+        it_behaves_like "is valid"
+      end
+      context "with a blank business name" do
+        before { allow(subject).to receive(:company_name).and_return("") }
+        it_behaves_like "is valid"
+      end
+    end
+
+    RSpec.shared_examples "limited company or limited liability partnership" do
+      context "with a registered company name" do
+        before { allow(subject).to receive(:registered_company_name).and_return(Faker::Company.name) }
+        it_behaves_like "business name is optional"
+      end
+      context "without a registered company name" do
+        before { allow(subject).to receive(:registered_company_name).and_return(nil) }
+        it_behaves_like "business name is required"
+      end
+    end
+
+    describe "#valid?" do
+      context "sole trader" do
+        before { allow(subject).to receive(:business_type).and_return("soleTrader") }
+        it_behaves_like "business name is optional"
+      end
+      context "limited company" do
+        before { allow(subject).to receive(:business_type).and_return("limitedCompany") }
+        it_behaves_like "limited company or limited liability partnership"
+      end
+      context "limited liability partnership" do
+        before { allow(subject).to receive(:business_type).and_return("limitedLiabilityPartnership") }
+        it_behaves_like "limited company or limited liability partnership"
+      end
+    end
+  end
+  # rubocop:enable Metrics/BlockLength
+end
