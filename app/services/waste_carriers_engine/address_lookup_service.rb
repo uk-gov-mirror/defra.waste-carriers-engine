05f229d48fbdc420e2cfebd08d2899d4cba63f1e
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-05-02 09:36:59 +0100

    Add benchmarking

--- app/services/waste_carriers_engine/address_lookup_service.rb
+++ app/services/waste_carriers_engine/address_lookup_service.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 require "rest-client"
+require "benchmark"
 
 module WasteCarriersEngine
   class AddressLookupService < BaseService
     def run(postcode)
-      DefraRuby::Address::EaAddressFacadeV11Service.run(postcode)
+      result = nil
+      execution_time = Benchmark.realtime do
+        result = DefraRuby::Address::EaAddressFacadeV11Service.run(postcode)
+      end
+
+      # Log the execution time
+      Rails.logger.info("AddressLookupService execution time: #{execution_time.round(4)} seconds")
+
+      result
     end
   end
 end
