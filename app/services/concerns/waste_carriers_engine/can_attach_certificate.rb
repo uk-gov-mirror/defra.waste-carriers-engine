ca335e5cd4e987b76b97023bebde63b8babf0684
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-09-29 15:50:06 +0100

    Allow wicked_pdf to use local file storage (#1443)

--- app/services/concerns/waste_carriers_engine/can_attach_certificate.rb
+++ app/services/concerns/waste_carriers_engine/can_attach_certificate.rb
@@ -19,7 +19,9 @@ module WasteCarriersEngine
           template: "waste_carriers_engine/pdfs/certificate",
           encoding: "UTF-8",
           layout: false,
-          locals: { presenter: certificate_presenter }
+          locals: { presenter: certificate_presenter },
+          enable_local_file_access: true,
+          allow: [WasteCarriersEngine::Engine.root.join("app", "assets", "images", "environment_agency_logo.png").to_s]
         )
       end
     end
