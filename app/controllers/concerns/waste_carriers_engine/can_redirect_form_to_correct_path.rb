f00ce35efbfb5475a42a48edada59796df0698a1
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-11-30 15:11:28 +0000

    [RUBY-2765] Fix routing bug in form_path with dynamic URL generation

--- app/controllers/concerns/waste_carriers_engine/can_redirect_form_to_correct_path.rb
+++ app/controllers/concerns/waste_carriers_engine/can_redirect_form_to_correct_path.rb
@@ -12,7 +12,15 @@ module WasteCarriersEngine
       # Get the path based on the workflow state, with token as params, ie:
       # new_state_name_path/:token or start_state_name_path?token=:token
       def form_path
-        send("new_#{@transient_registration.workflow_state}_path".to_sym, token: @transient_registration.token)
+        # Corrects a routing bug by dynamically determining the correct URL for a form.
+        # Tries to generate the path with `main_app`, falling back to `basic_app_engine` on failure.
+        # Ensures accurate path generation in contexts where default routing may fail,
+        # Workaround for creating incorrect routes when not specifying the
+        # url_helpers source in the main app
+
+        main_app.send("new_#{@transient_registration.workflow_state}_path".to_sym, token: @transient_registration.token)
+      rescue NoMethodError
+        basic_app_engine.send("new_#{@transient_registration.workflow_state}_path".to_sym, token: @transient_registration.token)
       end
     end
   end
