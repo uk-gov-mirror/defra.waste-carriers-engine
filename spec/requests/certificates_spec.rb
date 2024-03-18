e99dab213a8292a04624415e08eab6e5f4d3b677
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-03-18 13:47:22 +0000

    Make email validation case-insensitive in certificates controller and update tests (#1518)
    * Make email validation case-insensitive in certificates controller and update tests
    
    * [RUBY-3024] Refactor certificate request specs for rubocop

--- spec/requests/certificates_spec.rb
+++ spec/requests/certificates_spec.rb
@@ -14,11 +14,23 @@ RSpec.describe "Certificates" do
   let(:base_path) { "/#{registration.reg_identifier}/certificate" }
   let(:token_renewal_path) { "/#{registration.reg_identifier}/certificate_renew_token" }
   let(:token_renewal_sent_path) { "/#{registration.reg_identifier}/certificate_renewal_sent" }
+  let(:valid_email_uppercase) { valid_email.upcase }
+  let(:valid_email_mixed_case) { "ConTact@ExAmple.CoM" }
 
   describe "POST process_email" do
     context "with valid email" do
       it "sets the valid email in session and redirects to the certificate page" do
         post process_email_path, params: { email: valid_email }
+        expect(response).to redirect_to("#{base_path}?token=#{token}")
+      end
+
+      it "redirects to the certificate page with uppercase email" do
+        post process_email_path, params: { email: valid_email_uppercase }
+        expect(response).to redirect_to("#{base_path}?token=#{token}")
+      end
+
+      it "redirects to the certificate page with mixed case email" do
+        post process_email_path, params: { email: valid_email_mixed_case }
         expect(response).to redirect_to("#{base_path}?token=#{token}")
       end
     end
@@ -200,7 +212,6 @@ RSpec.describe "Certificates" do
     before { allow(WasteCarriersEngine::CertificateRenewalService).to receive(:run) }
 
     context "when the email is valid" do
-
       it "resets the token and redirects to the token renewal sent page" do
         post certificate_reset_token_path(reg_identifier: registration.reg_identifier, email: valid_email)
 
@@ -209,6 +220,16 @@ RSpec.describe "Certificates" do
         expect(response).to redirect_to(token_renewal_sent_path)
         follow_redirect!
         expect(response).to render_template("waste_carriers_engine/certificates/renewal_sent")
+      end
+
+      it "resets the token and redirects to the token renewal sent page with a uppercase email" do
+        post process_email_path, params: { email: valid_email_uppercase }
+        expect(response).to redirect_to("#{base_path}?token=#{token}")
+      end
+
+      it "resets the token and redirects to the token renewal sent page with a mixed case email" do
+        post process_email_path, params: { email: valid_email_mixed_case }
+        expect(response).to redirect_to("#{base_path}?token=#{token}")
       end
     end
 
